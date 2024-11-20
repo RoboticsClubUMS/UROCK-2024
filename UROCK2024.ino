6e310e513b48cd1a35afbdc082ec9a5a616724f5
Robotics Club UMS <120725657+RoboticsClubUMS@users.noreply.github.com>
2024-11-21 04:13:59 +0800

    Update UROCK2024.ino

--- UROCK2024.ino
+++ UROCK2024.ino
@@ -9,10 +9,13 @@ void setup() {
   Serial.begin(115200);
   
   // SETUP GYRO
-  setupGyro();
+  //setupGyro();
 
   // SETUP SERVO
   setupLeg();
+
+  readyLeg();
+  delay(3000);
 
   // SETUP OBJECT DETECTOR
   setupOBJD();
@@ -21,31 +24,33 @@ void setup() {
 }
 
 void loop() {
+
   updateGyro();
   getOBJD();
 
   if (counter == 3) counter = 0;
   if (counter_two == 3) counter = 0;
 
-    if (isRamp()) {
-      // MOVEMENT RAMP
-      // IF TOO SENSITIVE, EITHER DUMP THE SENSORS OR CALIBRATE AT FINEST
-    } else {
-      if (getAvgGyro() > 20) {
-  // MOVEMENT TO THE LEFT
-  // WORK WITH MECHA TO GET THE ANGLE
-      } else if (getAvgGyro() < -20) {
-  // MOVEMENT TO THE RIGHT
-  // WORK WITH MECHA TO GET THE ANGLE
-      } else {
-      // NORMAL MOVEMENT
-        L1Move(counter);
-        L2Move(counter_two);
-        L3Move(counter);
-        L4Move(counter_two);
-      }
-    }
+  // NORMAL MOVEMENT
+  if (isRamp()) {
+    // MOVEMENT RAMP
+    // IF TOO SENSITIVE, EITHER DUMP THE SENSORS OR CALIBRATE AT FINEST
+  } else {
 
+    if (getAvgGyro() > 20) {
+    // MOVEMENT TO THE LEFT
+    // WORK WITH MECHA TO GET THE ANGLE
+    } else if (getAvgGyro() < -20) {
+    // MOVEMENT TO THE RIGHT
+    // WORK WITH MECHA TO GET THE ANGLE
+    } else {
+      L1Move(counter);
+      L2Move(counter_two);
+      L3Move(counter);
+      L4Move(counter_two);
+    }
+  }
+  
   counter++;
   counter_two++;
   delay(200);
