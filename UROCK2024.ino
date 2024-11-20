64e9dc040d87da74b446497c0d326a1c93e170b8
Robotics Club UMS <120725657+RoboticsClubUMS@users.noreply.github.com>
2024-11-20 22:11:21 +0800

    Update UROCK2024.ino

--- UROCK2024.ino
+++ UROCK2024.ino
@@ -27,24 +27,24 @@ void loop() {
   if (counter == 3) counter = 0;
   if (counter_two == 3) counter = 0;
 
-  if (getAvgGyro() > 20) {
-    // MOVEMENT TO THE LEFT
-    // WORK WITH MECHA TO GET THE ANGLE
-  } else if (getAvgGyro() < -20) {
-    // MOVEMENT TO THE RIGHT
-    // WORK WITH MECHA TO GET THE ANGLE
-  } else {
-    // NORMAL MOVEMENT
     if (isRamp()) {
       // MOVEMENT RAMP
       // IF TOO SENSITIVE, EITHER DUMP THE SENSORS OR CALIBRATE AT FINEST
     } else {
-      L1Move(counter);
-      L2Move(counter_two);
-      L3Move(counter);
-      L4Move(counter_two);
+      if (getAvgGyro() > 20) {
+  // MOVEMENT TO THE LEFT
+  // WORK WITH MECHA TO GET THE ANGLE
+      } else if (getAvgGyro() < -20) {
+  // MOVEMENT TO THE RIGHT
+  // WORK WITH MECHA TO GET THE ANGLE
+      } else {
+      // NORMAL MOVEMENT
+        L1Move(counter);
+        L2Move(counter_two);
+        L3Move(counter);
+        L4Move(counter_two);
+      }
     }
-  }
 
   counter++;
   counter_two++;
