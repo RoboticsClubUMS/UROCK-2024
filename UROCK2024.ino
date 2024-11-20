8c2c8d994d3ae0883e99fe7441247e438aa31973
Robotics Club UMS <120725657+RoboticsClubUMS@users.noreply.github.com>
2024-11-20 20:26:43 +0800

    Update and rename untitled.ino to UROCK2024.ino

---
+++ UROCK2024.ino
@@ -0,0 +1,53 @@
+#include <main.h>
+
+int counter = 0;
+int counter_two = 1;
+
+bool flag = true;
+
+void setup() {
+  Serial.begin(115200);
+  
+  // SETUP GYRO
+  setupGyro();
+
+  // SETUP SERVO
+  setupLeg();
+
+  // SETUP OBJECT DETECTOR
+  setupOBJD();
+
+  updateGyro();
+}
+
+void loop() {
+  updateGyro();
+  getOBJD();
+
+  if (counter == 3) counter = 0;
+  if (counter_two == 3) counter = 0;
+
+  if (getAvgGyro() > 20) {
+    // MOVEMENT TO THE LEFT
+    // WORK WITH MECHA TO GET THE ANGLE
+  } else if (getAvgGyro() < -20) {
+    // MOVEMENT TO THE RIGHT
+    // WORK WITH MECHA TO GET THE ANGLE
+  } else {
+    // NORMAL MOVEMENT
+    if (isRamp()) {
+      // MOVEMENT RAMP
+      // IF TOO SENSITIVE, EITHER DUMP THE SENSORS OR CALIBRATE AT FINEST
+    } else {
+      L1Move(counter);
+      L2Move(counter_two);
+      L3Move(counter);
+      L4Move(counter_two);
+    }
+  }
+
+  counter++;
+  counter_two++;
+  delay(200);
+
+}
